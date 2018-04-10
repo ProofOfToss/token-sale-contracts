960f478e0bdb17b186e12d0ea36b119458e5ecf8
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-04-10 14:42:21 +0300

    Copied code from deploying file to Token.sol, removed allowanceToAllowBlocking, added freezed token check in Token.block(), fix other comments

--- TokenSale/Token/Token.sol
+++ TokenSale/Token/Token.sol
@@ -11,7 +11,9 @@ import "../ERC223ReceivingContract.sol";
 // Contract token
 contract Token is FreezingToken, MintableToken, MigratableToken, BurnableToken {
     string public constant name = "TOSS";
+
     string public constant symbol = "PROOF OF TOSS";
+
     uint8 public constant decimals = 18;
 
     mapping (address => mapping (address => bool)) public grantedToAllowBlocking; // Address of smart contract that can allow other contracts to block tokens
@@ -20,28 +22,39 @@ contract Token is FreezingToken, MintableToken, MigratableToken, BurnableToken {
 
     event TokenOperationEvent(string operation, address indexed from, address indexed to, uint256 value, address indexed _contract);
 
-    /**
-     * @dev Transfer the specified amount of tokens to the specified address.
-     *      Invokes the `tokenFallback` function if the recipient is a contract.
-     *      The token transfer fails if the recipient is a contract
-     *      but does not implement the `tokenFallback` function
-     *      or the fallback function to receive funds.
-     *
-     * @param _to    Receiver address.
-     * @param _value Amount of tokens that will be transferred.
-     * @param _data  Transaction metadata.
-     */
-    function transferToContract(address _to, uint _value, bytes _data) public returns (bool) {
-        // Standard function transfer similar to ERC20 transfer with no _data .
-        // Added due to backwards compatibility reasons .
-        uint codeLength;
+
+    //!!!Добавил модификатор
+    modifier contractOnly(address _to) {
+        uint256 codeLength;
 
         assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_to)
+        // Retrieve the size of the code on target address, this needs assembly .
+        codeLength := extcodesize(_to)
         }
 
         require(codeLength > 0);
+
+        _;
+    }
+
+    /**
+    * @dev Transfer the specified amount of tokens to the specified address.
+    * Invokes the `tokenFallback` function if the recipient is a contract.
+    * The token transfer fails if the recipient is a contract
+    * but does not implement the `tokenFallback` function
+    * or the fallback function to receive funds.
+    *
+    * @param _to Receiver address.
+    * @param _value Amount of tokens that will be transferred.
+    * @param _data Transaction metadata.
+    */
+
+    //!!! Добавил модификатор public
+    function transferToContract(address _to, uint _value, bytes _data) public contractOnly(_to) returns (bool) {
+        // Standard function transfer similar to ERC20 transfer with no _data .
+        // Added due to backwards compatibility reasons .
+
+        //!!!Убал проверку, теперь она в модификаторе
 
         super.transfer(_to, _value);
 
@@ -54,14 +67,10 @@ contract Token is FreezingToken, MintableToken, MigratableToken, BurnableToken {
     // @brief Allow another contract to allow another contract to block tokens. Can be revoked
     // @param _spender another contract address
     // @param _value amount of approved tokens
-    function grantToAllowBlocking(address _contract, bool permission) {
-        uint codeLength;
+    //!!! Добавил модификатор public
+    function grantToAllowBlocking(address _contract, bool permission) contractOnly(_contract) public {
 
-        assembly { // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_contract)
-        }
-
-        if (codeLength <= 0) throw; // Only smart contracts allowed
+        //!!!Убал проверку, теперь она в модификаторе
 
         grantedToAllowBlocking[msg.sender][_contract] = permission;
 
@@ -71,40 +80,33 @@ contract Token is FreezingToken, MintableToken, MigratableToken, BurnableToken {
     // @brief Allow another contract to block tokens. Can't be revoked
     // @param _owner tokens owner
     // @param _contract another contract address
-    function allowBlocking(address _owner, address _contract) {
-        uint codeLength;
+    //!!! Добавил модификатор public
+    function allowBlocking(address _owner, address _contract) contractOnly(_contract) public {
 
-        assembly { // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(_contract)
-        }
+        //!!! Заменил if(condition) throw на require
 
-        if (codeLength <= 0) throw; // Only smart contracts allowed
-        if (_contract == msg.sender) throw;
-        if (_contract == _owner) throw;
-        if (! grantedToAllowBlocking[_owner][msg.sender]) throw;
+        require(_contract != msg.sender && _contract != owner);
+
+        require(grantedToAllowBlocking[_owner][msg.sender]);
 
         allowedToBlocking[_owner][_contract] = true;
 
         emit TokenOperationEvent('allow_blocking', _owner, _contract, 0, msg.sender);
     }
 
-    // @brief Check if contract is granted to allow blocking to other contracts
-    // @param _owner owner of allowance
-    // @param _spender spender contract
-    // @return the rest of allowed tokens
-    function allowanceToAllowBlocking(address _owner, address _contract) constant returns (bool granted) {
-        return grantedToAllowBlocking[_owner][_contract];
-    }
-
     // @brief Blocks tokens
     // @param _blocking The address of tokens which are being blocked
     // @param _value The blocked token count
-    function block(address _blocking, uint256 _value) whenNotPaused(_blocking) {
-        if (! allowedToBlocking[_blocking][msg.sender]) throw;
-        if (balances[_blocking] < _value || _value <= 0) throw;
+    //!!! Добавил модификатор public
+    //!!! Внимание! название метода перекрывает ключевое слово block!
+    function blockTokens(address _blocking, uint256 _value) whenNotPaused(_blocking) public {
+        //!!! Заменил if(condition) throw на require
+        require(allowedToBlocking[_blocking][msg.sender]);
 
-        balances[_blocking] -= _value;
-        blocked[_blocking][msg.sender] += _value;
+        require(balanceOf(_blocking) >= freezedTokenOf(_blocking).add(_value) && _value > 0);
+
+        balances[_blocking] = balances[_blocking].sub(_value);
+        blocked[_blocking][msg.sender] = blocked[_blocking][msg.sender].add(_value);
 
         emit TokenOperationEvent('block', _blocking, 0, _value, msg.sender);
     }
@@ -113,13 +115,14 @@ contract Token is FreezingToken, MintableToken, MigratableToken, BurnableToken {
     // @param _blocking The address of tokens which are blocked
     // @param _unblockTo The address to send to the blocked tokens after unblocking
     // @param _value The blocked token count to unblock
-    function unblock(address _blocking, address _unblockTo, uint256 _value) whenNotPaused(_unblockTo) {
-        if (blocked[_blocking][msg.sender] == 0) throw;
-        if (! allowedToBlocking[_blocking][msg.sender]) throw;
-        if (blocked[_blocking][msg.sender] < _value) throw;
+    //!!! Добавил модификатор public
+    function unblockTokens(address _blocking, address _unblockTo, uint256 _value) whenNotPaused(_unblockTo) public {
+        //!!! Заменил if(condition) throw на require
+        require(allowedToBlocking[_blocking][msg.sender]);
+        require(blocked[_blocking][msg.sender] >= _value && _value > 0);
 
-        blocked[_blocking][msg.sender] -= _value;
-        balances[_unblockTo] += _value;
+        blocked[_blocking][msg.sender] = blocked[_blocking][msg.sender].sub(_value);
+        balances[_unblockTo] = balances[_unblockTo].add(_value);
 
         emit TokenOperationEvent('unblock', _blocking, _unblockTo, _value, msg.sender);
     }
