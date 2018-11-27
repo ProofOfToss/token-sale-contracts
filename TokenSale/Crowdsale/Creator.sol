9c3970809bd000f0822cee3dc7e2fbbaf60cbb84
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-11-27 10:55:44 +0300

    37780 periodic allocation

--- TokenSale/Crowdsale/Creator.sol
+++ TokenSale/Crowdsale/Creator.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.21;
 
 import './RefundVault.sol';
 import './AllocationToss.sol';
+import './PeriodicAllocation.sol';
 import '../Token/Token.sol';
 
 contract Creator{
@@ -15,6 +16,12 @@ contract Creator{
 
     function createAllocation(Token _token, uint256 _unlockPart1, uint256 _unlockPart2) external returns (AllocationTOSS) {
         AllocationTOSS allocation = new AllocationTOSS(_token,_unlockPart1,_unlockPart2);
+        allocation.transferOwnership(msg.sender);
+        return allocation;
+    }
+
+    function createPeriodicAllocation(Token _token, uint256 _unlockStart) external returns (PeriodicAllocation) {
+        PeriodicAllocation allocation = new PeriodicAllocation(_token,_unlockStart);
         allocation.transferOwnership(msg.sender);
         return allocation;
     }
