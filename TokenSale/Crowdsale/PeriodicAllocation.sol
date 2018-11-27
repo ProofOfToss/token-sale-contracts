3691ea27e4d45fbf66046fc584b18cfe4a01a252
Nickolay Savchenko <n.savchenko@axioma.lv>
2018-11-27 10:55:44 +0300

    37780 periodic allocation

---
+++ TokenSale/Crowdsale/PeriodicAllocation.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.21;
+
+import '../Ownable.sol';
+import '../SafeMath.sol';
+import '../Token/ERC20Basic.sol';
+
+// (B)
+// The contract for freezing tokens for the players and investors..
+contract PeriodicAllocation is Ownable {
+    using SafeMath for uint256;
+
+    struct Share {
+        uint256 proportion;
+        uint256 periods;
+        uint256 periodLength;
+    }
+
+    // How many days to freeze from the moment of finalizing ICO
+    uint256 public unlockStart;
+    uint256 public totalShare;
+
+    mapping(address => Share) public shares;
+    mapping(address => uint256) public unlocked;
+
+    ERC20Basic public token;
+
+    function PeriodicAllocation(ERC20Basic _token, uint256 _unlockStart) public {
+        unlockStart = _unlockStart;
+        token = _token;
+    }
+
+    function addShare(address _beneficiary, uint256 _proportion, uint256 _periods, uint256 _periodLength) onlyOwner external {
+        shares[_beneficiary] = Share(shares[_beneficiary].proportion.add(_proportion),_periods,_periodLength);
+        totalShare = totalShare.add(_proportion);
+    }
+
+    // If the time of freezing expired will return the funds to the owner.
+    function unlockFor(address _owner) public {
+        require(now >= unlockStart);
+        uint256 share = shares[_owner].proportion;
+        uint256 periodsSinceUnlockStart = (now.sub(unlockStart)).div(shares[_owner].periodLength);
+
+        if (periodsSinceUnlockStart < shares[_owner].periods) {
+            share = share.div(shares[_owner].periods).mul(periodsSinceUnlockStart);
+        }
+
+        share = share.sub(unlocked[_owner]);
+
+        if (share > 0) {
+            uint256 unlockedToken = token.balanceOf(this).mul(share).div(totalShare);
+            totalShare = totalShare.sub(share);
+            unlocked[_owner] += share;
+            token.transfer(_owner,unlockedToken);
+        }
+    }
+}
